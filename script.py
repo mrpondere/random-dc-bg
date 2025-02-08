5c83d36b0ceb5c76f4a3aa60423f51494fec6ada
Pondere <pondere@outlook.com>
2025-02-08 06:18:00 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,6 @@
-print('test')
+import random
+
+colors = ['red', 'green', 'blue', 'yellow', 'purple']
+
+with open('background.css', 'w') as file:
+    file.write(f'body {{ background-color: {colors[random.randint(0, 4)]}; }}')
