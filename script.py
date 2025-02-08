5289ca38030d90c6d041177e08bf0fc0ce180d8c
Pondere <pondere@outlook.com>
2025-02-08 07:09:46 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,10 +1,12 @@
 import random
+import time
 
 walls = []
 with open('walls.txt', 'r') as file:
     walls = file.read().splitlines()
 
 with open('background.css', 'w') as file:
+    file.write('/* Generated at ' + time.strftime('%Y-%m-%d %H:%M:%S') + ' */\n')
     file.write(':root {\n')
     file.write('--background-image: url("' + random.choice(walls) + '");\n')
     file.write('}\n')
